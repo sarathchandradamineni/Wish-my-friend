e68cc3073f51ab09121ba7e22cc089fff6217a98
Sarath Chandra Damineni <sarathchandradamineni@gmail.com>
2021-02-05 09:06:14 +0100

    Initial Commit

---
+++ Wish my friendTests/Wish_my_friendTests.swift
@@ -0,0 +1,33 @@
+//
+//  Wish_my_friendTests.swift
+//  Wish my friendTests
+//
+//  Created by Sarath Chandra Damineni on 2021-02-05.
+//
+
+import XCTest
+@testable import Wish_my_friend
+
+class Wish_my_friendTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
